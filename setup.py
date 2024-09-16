4ab460b6c3ac86ab647a354afbad5b3cc07d5970
Hierosme <ben@galaxie.eu.org>
2024-09-16 10:16:14 +0000

    Migrate to pyproject.toml

--- setup.py
+++ setup.py
@@ -1,8 +1,38 @@
-import pathlib
-from setuptools import setup, find_packages
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-ROOT = pathlib.Path('.')
-README = (ROOT / "README.md").read_text()
+import codecs
+import os
+
+from setuptools import setup
+
+
+def read(rel_path):
+    with codecs.open(
+        os.path.join(os.path.abspath(os.path.dirname(__file__)), rel_path), "r"
+    ) as fp:
+        return fp.read()
+
+
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith("__version__"):
+            delimiter = '"' if '"' in line else "'"
+            return line.split(delimiter)[1]
+    else:
+        raise RuntimeError("Unable to find version string.")
+
+
+if __name__ == "__main__":
+
+    if os.environ.get("APPLICATION_VERSION"):
+        version = os.environ["APPLICATION_VERSION"]
+    else:
+        version = get_version("ImageGoNord/__init__.py")
+    print(f"Setup version is: {version}")
+    setup(
+        version=version,
+    )
 
 setup(
     name="image-go-nord",
@@ -11,18 +41,29 @@ setup(
     long_description=README,
     long_description_content_type="text/markdown",
     url="https://github.com/schroedinger-Hat/ImageGoNord-pip",
-    download_url = 'https://github.com/schroedinger-Hat/ImageGoNord-pip/releases',
-    keywords = ['nordtheme', 'pillow', 'image', 'conversion', 'rgb', 'color-scheme', 'color-palette', 'linux-rice', 'gruvbox', 'catpuccin'],
+    download_url="https://github.com/schroedinger-Hat/ImageGoNord-pip/releases",
+    keywords=[
+        "nordtheme",
+        "pillow",
+        "image",
+        "conversion",
+        "rgb",
+        "color-scheme",
+        "color-palette",
+        "linux-rice",
+        "gruvbox",
+        "catpuccin",
+    ],
     author="Schroedinger Hat",
     author_email="dev@schroedinger-hat.org",
     license="AGPL-3.0",
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Build Tools',
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
         "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7"
+        "Programming Language :: Python :: 3.7",
     ],
     project_urls={
         "Homepage": "https://ign.schroedinger-hat.org",
@@ -30,11 +71,9 @@ setup(
         "Bug Reports": "https://github.com/schroedinger-Hat/ImageGoNord-pip/issues",
     },
     packages=find_packages(),
-    package_data={'': ['*.txt', 'palettes/*.txt']},
+    package_data={"": ["*.txt", "palettes/*.txt"]},
     include_package_data=True,
     install_requires=["Pillow", "ffmpeg-python", "numpy", "requests"],
-    extras_require = {
-        'AI':  ["torch", "scikit-image", "torchvision"]
-    },
-    python_requires=">=3.5"
+    extras_require={"AI": ["torch", "scikit-image", "torchvision"]},
+    python_requires=">=3.5",
 )
